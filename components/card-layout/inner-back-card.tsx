bc8e869094702c388f347f68228b81fbf47e5538
Luisotee <luisotaviomgr1@gmail.com>
2022-09-14 19:11:56 -0300

    changed CVCText and innerbackcard to JSx to avoid mantine errors

--- components/card-layout/inner-back-card.tsx
+++ components/card-layout/inner-back-card.tsx
@@ -1,11 +1,19 @@
 import { Text } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import { CardViewProps } from "../../interface";
 import { CardSmallLetters, CVCText } from "../../styles/css-components";
 
 export function InnerBackCard({ cardValue }: CardViewProps) {
+  const isMobile = useMediaQuery("(min-width: 1080px)");
   return (
     <CardSmallLetters>
-      <Text className="CVCText">{cardValue.cardCVC}</Text>
+      {isMobile ? (
+        <Text className="CVCText">{cardValue.cardCVC}</Text>
+      ) : (
+        <Text align="right" className="CVCTextMobile">
+          {cardValue.cardCVC}
+        </Text>
+      )}
     </CardSmallLetters>
   );
 }
